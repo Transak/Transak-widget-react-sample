d3108c950520b522f62f370b9dc16ec443f30280
Yeshu Agarwal <agarwal.yeshu@gmail.com>
2020-07-03 00:24:54 +0530

    Integrate Transak SDK
    Added Transak SDK so that our partners refer the sample code.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import transakSDK from "@transak/transak-sdk";
+
+const settings = {
+    apiKey: 'cf5868eb-a8bb-45c8-a2db-4309e5f8b412',  // Your API Key
+    environment: 'STAGING', // STAGING/PRODUCTION
+    defaultCryptoCurrency: 'ETH',
+    themeColor: '000000', // App theme color
+    hostURL: window.location.origin,
+    widgetHeight: "700px",
+    widgetWidth: "500px",
+}
+
+export function openTransak() {
+    const transak = new transakSDK(settings);
+
+    transak.init();
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <button onClick={() => openTransak()}>
+                    open trasak
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
